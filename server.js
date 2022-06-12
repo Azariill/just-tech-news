74ac235513b85ba9e884cbb466f037cccdb94470
Azarill <heierms@gmail.com>
2022-06-11 22:45:54 -0500

    updated formatting and added helper functions

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const exphbs = require('express-handlebars');
 const express = require("express");
 const path = require("path");
 const routes = require("./controllers");
-const hbs = exphbs.create({});
+
 const sequelize = require("./config/connection");
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -17,7 +17,8 @@ const sess = {
     db: sequelize
   })
 };
-
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({helpers});
 
 const PORT = process.env.PORT || 3001;
 
